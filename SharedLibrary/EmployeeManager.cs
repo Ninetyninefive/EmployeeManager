5446c2808d1dd5bb6b2a99c1c28147bc5c8004c8
NinetynineFive <casper.jaller@gmail.com>
2020-11-18 23:00:09 +0100

    Sixth: Setup of a SharedLibrary
    It works

---
+++ SharedLibrary/EmployeeManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SharedLibrary
+{
+    public static class EmployeeManager
+    {
+        public static bool StartsWithUpper(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            return false;
+    
+            char ch = str[0];
+            return char.IsUpper(ch);
+        }
+    }
+}
