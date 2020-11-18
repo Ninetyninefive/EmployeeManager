84965319fffc2064f1bba796846665ea990493fb
NinetynineFive <casper.jaller@gmail.com>
2020-11-18 23:04:26 +0100

    AdminUI

---
+++ AdminUI/AdminUI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace AdminUI
+{
+    class AdminUI
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var result = SharedLibrary.EmployeeManager.StartsWithUpper("Hello");
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+    }
+}
